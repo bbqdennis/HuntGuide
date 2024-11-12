49e9b51644f0fcf17f2eef6db2761c2cf24712c9
Dennis Cheng <bbqdennis@yahoo.com.hk>
2024-11-12 16:10:37 +0000

    Initial Commit

---
+++ HuntGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HuntGuide
+//
+//  Created by Dennis Cheng on 12/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HuntGuideTests/HuntGuideTests.swift
@@ -0,0 +1,17 @@
+//
+//  HuntGuideTests.swift
+//  HuntGuideTests
+//
+//  Created by Dennis Cheng on 12/11/2024.
+//
+
+import Testing
+@testable import HuntGuide
+
+struct HuntGuideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
